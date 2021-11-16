d693c75d2ed3894c722bb38b07cfc236ed64c050
rodrigo govea <rodry.gm.isc@live.com.mx>
2021-11-16 11:31:54 -0600

    JS

---
+++ JS/scripts.js
@@ -0,0 +1,146 @@
+//alert("Hola mundo desde JS, desde archivo externo...");
+
+// var nombre = "Rodrigo";
+// var edad = "34";
+// var unBoleano = false;
+// console.log(typeof(nombre));
+// console.log(typeof(edad));
+// console.log(typeof(unBoleano));
+
+const nombreProducto = "Monitor";
+const precioProducto = 5000;
+const categoriaProducto = "Gamer";
+
+// console.log(nombreProducto);
+// console.log(precioProducto);
+// console.log(categoriaProducto);
+
+const producto = {
+    nombre : "Monitor",
+    precio : 5000,
+    categoria : "Gamer"
+}
+
+// console.log(producto);
+
+console.log(producto.nombre);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*variables*/
+// let nombre = "Rodrigo";
+// let edad = 34;
+// let esCasado = "si";
+
+// console.log(esCasado);
+
+// esCasado = true;
+
+// console.log(nombre,edad, esCasado);
+
+//metodologías para nombrar varianles
+/*
+
+var nombre_producto = "monitor gamer"; //underscore
+var nombreProducto = "monitos gamer"; //camelcase
+var NombreProducto = "monitor gamer"; //Pascal Case
+var nombreproducto =  "monitor gamer";
+*/
+ 
+// const nombre = "Rodrigo";
+// const edad = 34;
+
+// // edad = 40;
+
+// let apellido;
+
+
+// const producto = "Monitor gamer 20\"";
+// console.log(typeof producto);
+// const producto2 = String("Monitor gamer HD");
+// console.log(typeof producto2);
+// const producto3 = new String("Monitor gamer super HD");
+// console.log(typeof producto3);
+
+
+// const producto = "Monitor de 20\"";
+// const producto2 = "Monitor HD";
+
+// console.log(producto.length);
+// //IndexOf
+
+// console.log(producto.indexOf("20"));
+// console.log(producto2.indexOf("20"));
+
+// //include
+
+// console.log(producto.includes("20"));
+// console.log(producto2.includes("20"));
+
+
+// const nombre = "Rodrigo";
+// console.log(Nombre);
+
+
+// const nombreProducto = "Monitor AlienWare 40\"";
+// const precioProducto = 10000;
+// const categoriaProducto = "Gamer";
+
+// console.log(nombreProducto,precioProducto,categoriaProducto);
+
+// const producto = {
+//     nombre : "Monitor AlienWare 40\"",
+//     precio : 10000,
+//     categoria : 'Gamer'
+// }
+
+// const precioProducto = producto.precio;
+
+// console.log(precioProducto);
+
+//destructuring de objetoss
+
+// const producto = {
+//     nombre : "monitor HD",
+//     precio : 4600,
+//     color : "negro"
+// }
+// const medidas = {
+//     alto : "20cm",
+//     ancho : "40cm",
+//     peso : "3kg"
+// }
+
+// const nuevoProducto = {...producto, ...medidas};
+
+// console.log(nuevoProducto);
+
+
+
+
+
